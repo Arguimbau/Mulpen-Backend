4ea5c32c95fdbc4f96d3fe7b39246f136c224fea
ArthurOS <arth3608@stud.kea.dk>
2023-11-20 10:16:14 +0100

    Initial commit

---
+++ src/main/java/dk/kea/mulpenbackend/MulpenBackendApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.mulpenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulpenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MulpenBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/kea/mulpenbackend/MulpenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.mulpenbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulpenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
