fc8ef6384a2e8036be3fd7010de1dcec6991efaa
DizzyMoon <mikkel.simons1@gmail.com>
2023-11-30 13:10:58 +0100

    navigation should work

---
+++ src/main/java/dk/kea/mulpenbackend/api/ViewController.java
@@ -0,0 +1,33 @@
+package dk.kea.mulpenbackend.api;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.io.IOException;
+
+@Controller
+@EnableWebMvc
+public class ViewController {
+
+
+    @GetMapping("/videos")
+    public void forwardToViewVideoPage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletContext context = request.getServletContext();
+        RequestDispatcher requestDispatcher = context.getRequestDispatcher("/viewVideo.html");
+        requestDispatcher.forward(request, response);
+    }
+
+    @GetMapping("/upload")
+    public void forwardToUploadMediaPage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletContext context = request.getServletContext();
+        RequestDispatcher requestDispatcher = context.getRequestDispatcher("/uploadMedia.html");
+        requestDispatcher.forward(request, response);
+    }
+}
--- src/main/java/dk/kea/mulpenbackend/config/WebConfig.java
+++ src/main/java/dk/kea/mulpenbackend/config/WebConfig.java
@@ -2,16 +2,19 @@ package dk.kea.mulpenbackend.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
     @Value("${frontend.path}")
     private String frontendPath;
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry){
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
-                .addResourceLocations("file:" + frontendPath);
+                .addResourceLocations("file:" + frontendPath + "/");
     }
 }
