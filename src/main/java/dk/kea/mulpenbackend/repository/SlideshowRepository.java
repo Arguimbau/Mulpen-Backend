56bbedc4b8bf4370867b85ce66a347d0bde62d12
Geargrind <lass347s@stud.kea.dk>
2023-12-07 10:48:15 +0100

    Changed Mediamodel usages to SlideshowModel.

--- src/main/java/dk/kea/mulpenbackend/repository/SlideshowRepository.java
+++ src/main/java/dk/kea/mulpenbackend/repository/SlideshowRepository.java
@@ -1,11 +1,12 @@
 package dk.kea.mulpenbackend.repository;
 
 import dk.kea.mulpenbackend.model.MediaModel;
+import dk.kea.mulpenbackend.model.SlideshowModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SlideshowRepository extends JpaRepository<MediaModel, Long> {
+public interface SlideshowRepository extends JpaRepository<SlideshowModel, Long> {
   public boolean existsByFilePath(String filepath);
 
 }
