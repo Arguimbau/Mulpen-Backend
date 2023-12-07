e1423ef673a85c41bb52c23d841027beb29eb08e
ArthurOS <arth3608@stud.kea.dk>
2023-12-07 13:51:26 +0100

    Updated pictures

--- src/main/java/dk/kea/mulpenbackend/config/InitData.java
+++ src/main/java/dk/kea/mulpenbackend/config/InitData.java
@@ -27,42 +27,42 @@ public class InitData implements CommandLineRunner
 
         UserModel user2 = new UserModel();
         user2.setPassword("1234");
-        user2.setUsername("user1");
+        user2.setUsername("Amalie");
         user2.setName("Amalie Vixø Drøscher");
         user2.setPhoneNumber("+45 2064 9953");
         user2.setTitle("Administrerende direktør, Tilrættelægger, fotograf og klipper");
         user2.setEmail("amalie@mulpen.dk");
-        user2.setFilePath("images/profile1.jpg");
+        user2.setFilePath("images/Amalie.jpg");
         userService.save(user2);
 
         UserModel user3 = new UserModel();
         user3.setPassword("1234");
-        user3.setUsername("user2");
+        user3.setUsername("Konrad");
         user3.setName("Konrad Jon");
         user3.setPhoneNumber("+45 3131 9391");
         user3.setTitle("Underdirektør, Producer og økonomiansvarlig");
         user3.setEmail("konrad@mulpen.dk");
-        user3.setFilePath("images/profile2.jpg");
+        user3.setFilePath("images/Konrad.jpg");
         userService.save(user3);
 
         UserModel user4 = new UserModel();
         user4.setPassword("1234");
-        user4.setUsername("user3");
+        user4.setUsername("Emil");
         user4.setName("Emil Holmsteen");
         user4.setPhoneNumber("+45 7170 0678");
         user4.setTitle("Co-founder Skuespiller og idéudvikler");
         user4.setEmail("emil@mulpen.dk");
-        user4.setFilePath("images/profile3.jpg");
+        user4.setFilePath("images/Emil.jpg");
         userService.save(user4);
 
         UserModel user5 = new UserModel();
         user5.setPassword("1234");
-        user5.setUsername("user4");
+        user5.setUsername("Gustav");
         user5.setName("Gustav Halfdan");
         user5.setPhoneNumber("+45 2032 1659");
         user5.setTitle("Co-founder Komiker og idéudvikler");
         user5.setEmail("gustav@mulpen.dk");
-        user5.setFilePath("images/profile4.jpg");
+        user5.setFilePath("images/Gustav.jpg");
         userService.save(user5);
     }
 }
