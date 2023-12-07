a5f2daae46ed6603e78ac9fcc62afb1ca830b350
weix0015 <weix0015@stud.kea.dk>
2023-12-07 10:45:49 +0100

    added SlideshowModel.java

--- src/main/java/dk/kea/mulpenbackend/model/SlideshowModel.java
+++ src/main/java/dk/kea/mulpenbackend/model/SlideshowModel.java
@@ -11,13 +11,13 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "media")
-public class MediaModel {
+@Table(name = "slideshow_images")
+public class SlideshowModel {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String type;
   private String filePath;
-  private String description;
 }
